1e72548be5cd16c3e8a6267261a8397dab9d708d
ImDuong <ImDuong@users.noreply.github.com>
2023-12-14 17:42:13 +0000

    deploy: 0e1a0078505a19239e2b8f6c19877b2c254e854e

---
+++ components/storybox/storybox.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var storyBox = document.getElementById("story-box");
+  var storyText = document.getElementById("story-content");
+  var curStoryIdx = 0;
+  var storyLines = [];
+
+  if (storyPath == null || storyPath.length == 0) {
+    storyPath = "demo/404-room.json"
+  }
+
+  fetch(`../../../assets/storylines/${storyPath}`)
+    .then((response) => response.json())
+    .then((data) => {
+      storyLines = data;
+
+      storyText.textContent = storyLines[curStoryIdx]["text"];
+      curStoryIdx++;
+
+      setTimeout(() => {
+        storyBox.style.visibility = "visible";
+
+        storyBox.addEventListener("click", function () {
+          if (storyLines.length == 0 || curStoryIdx < 0) {
+            return;
+          }
+          if (curStoryIdx < storyLines.length) {
+            storyText.textContent = storyLines[curStoryIdx]["text"];
+            curStoryIdx++;
+          } else {
+            // if the last story line has enabled "loop" = true, the story box will never disappear
+            const curStoryLine = storyLines[curStoryIdx - 1];
+            if ("loop" in curStoryLine && curStoryLine["loop"] === true) {
+              curStoryIdx--;
+            } else {
+              storyBox.style.display = "none";
+            }
+          }
+        });
+      }, 2000);
+    })
+    .catch((error) => console.error("Error loading JSON:", error));
+});
---
+++ rooms/main-room/index.js
@@ -0,0 +1,2 @@
+// set path to story lines for storybox
+var storyPath = "demo/main-room.json"
