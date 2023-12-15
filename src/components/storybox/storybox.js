9ae6f04e663c730ab0a60ff035803fe03876aedc
ImDuong <shiroyasha699969@gmail.com>
2023-12-16 00:43:29 +0700

    ♻️ refactor: loading storylines to storybox & ✨show storylines when clicking doors

--- src/components/storybox/storybox.js
+++ src/components/storybox/storybox.js
@@ -1,42 +1,82 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var storyBox = document.getElementById("story-box");
-  var storyText = document.getElementById("story-content");
-  var curStoryIdx = 0;
-  var storyLines = [];
+var storyBox = document.createElement('div');
+var storyText;
+var curStoryIdx = 0;
 
+document.addEventListener("DOMContentLoaded", async function () {
+  storyBox = document.getElementById("story-box");
+  storyText = document.getElementById("story-content");
+});
+
+async function readStoryLines(storyPath) {
   if (storyPath == null || storyPath.length == 0) {
-    storyPath = "demo/404-room.json"
+    storyPath = "demo/404-room.json";
+  }
+  const readPath = `@@webRoot/../assets/storylines/${storyPath}`;
+  try {
+    const response = await fetch(readPath);
+    if (!response.ok) {
+      throw new Error(`error status code ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error(`Error loading storylines from ${readPath}`, error);
+  }
+}
+
+function loadStoryLines(storyLines) {
+  // read from start
+  curStoryIdx = 0
+
+  function loadNextStoryLine() {
+    if (storyLines.length == 0 || curStoryIdx < 0) {
+      storyBox.removeEventListener("click", loadNextStoryLine);
+      return;
+    }
+    if (curStoryIdx < storyLines.length) {
+      readAStoryLine()
+      return
+    }
+
+    // if the last story line has enabled "loop" = true, the story box will never disappear
+    const curStoryLine = storyLines[curStoryIdx - 1];
+    if ("loop" in curStoryLine && curStoryLine["loop"] === true) {
+      curStoryIdx--;
+    } else {
+      hideStoryBox();
+      storyBox.removeEventListener("click", loadNextStoryLine);
+    }
   }
 
-  fetch(`@@webRoot/../assets/storylines/${storyPath}`)
-    .then((response) => response.json())
-    .then((data) => {
-      storyLines = data;
+  function readAStoryLine() {
+    if (curStoryIdx >= storyLines.length) {
+      return;
+    }
+    storyText.textContent = storyLines[curStoryIdx]["text"];
+    curStoryIdx++;
+  }
 
-      storyText.textContent = storyLines[curStoryIdx]["text"];
-      curStoryIdx++;
+  setTimeout(() => {
+    readAStoryLine()
+    showStoryBox();
 
-      setTimeout(() => {
-        storyBox.style.visibility = "visible";
+    storyBox.addEventListener("click", loadNextStoryLine);
+  }, 2000);
+}
 
-        storyBox.addEventListener("click", function () {
-          if (storyLines.length == 0 || curStoryIdx < 0) {
-            return;
-          }
-          if (curStoryIdx < storyLines.length) {
-            storyText.textContent = storyLines[curStoryIdx]["text"];
-            curStoryIdx++;
-          } else {
-            // if the last story line has enabled "loop" = true, the story box will never disappear
-            const curStoryLine = storyLines[curStoryIdx - 1];
-            if ("loop" in curStoryLine && curStoryLine["loop"] === true) {
-              curStoryIdx--;
-            } else {
-              storyBox.style.display = "none";
-            }
-          }
-        });
-      }, 2000);
-    })
-    .catch((error) => console.error("Error loading JSON:", error));
-});
+function isStoryLineEnd(storyLines) {
+  return curStoryIdx >= storyLines.length
+}
+
+function showStoryBox() {
+  if (storyBox == null) {
+    return
+  }
+  storyBox.style.visibility = "visible";
+}
+
+function hideStoryBox() {
+  if (storyBox == null) {
+    return
+  }
+  storyBox.style.visibility = "hidden";
+}
--- src/rooms/main-room/index.js
+++ src/rooms/main-room/index.js
@@ -1,2 +1,28 @@
-// set path to story lines for storybox
-var storyPath = "demo/main-room.json"
+// set path to intial story lines for storybox
+const intialStoryPath = "demo/main-room.json";
+const leftRoomStoryPath = "navigation/main-room-left-door.json";
+var leftRoomClickCnt = 0;
+var leftRoomStoryLines = [];
+
+document.addEventListener("DOMContentLoaded", async function () {
+  var intialStoryLines = await readStoryLines(intialStoryPath);
+  loadStoryLines(intialStoryLines);
+
+  var leftDoorWrapper = document.getElementById("left-door-wrapper");
+  var rightDoorWrapper = document.getElementById("right-door-wrapper");
+
+  leftDoorWrapper.addEventListener("click", async function () {
+    if (leftRoomClickCnt == 0) {
+        leftRoomStoryLines = await readStoryLines(leftRoomStoryPath);
+        loadStoryLines(leftRoomStoryLines);
+        leftRoomClickCnt++
+        return
+    }
+    if (!isStoryLineEnd(leftRoomStoryLines)) {
+        leftRoomClickCnt++
+        return
+    }
+    
+    window.location.href = "../room-happiness/index.html";
+  });
+});
