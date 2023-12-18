f1801f47040d7803eaf6236dddcc5fbc078d34c3
duongtt0901 <duongtt0901@users.noreply.github.com>
2023-12-18 14:30:18 +0000

    deploy: a7e53590ef0c7529c42ca875fb20a73d29506227

--- dist/components/storybox/storybox.js
+++ dist/components/storybox/storybox.js
@@ -1,42 +1,91 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var storyBox = document.getElementById("story-box");
-  var storyText = document.getElementById("story-content");
-  var curStoryIdx = 0;
-  var storyLines = [];
+var storyBoxOverlay = document.createElement("div");
+var storyBox = document.createElement("div");
+var storyText;
+var curStoryIdx = 0;
 
+document.addEventListener("DOMContentLoaded", function () {
+  storyBoxOverlay = document.getElementById("story-box-overlay");
+  storyBox = document.getElementById("story-box");
+  storyText = document.getElementById("story-content");
+});
+
+async function fetchStoryLines(storyPath) {
   if (storyPath == null || storyPath.length == 0) {
-    storyPath = "demo/404-room.json"
+    storyPath = "demo/404-room.json";
+  }
+  const readPath = `../../../assets/storylines/${storyPath}`;
+  try {
+    const response = await fetch(readPath);
+    if (!response.ok) {
+      throw new Error(`error status code ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error(`Error loading storylines from ${readPath}`, error);
+  }
+}
+
+function viewStoryLines(storyLines) {
+  // read from start
+  curStoryIdx = 0;
+
+  function viewNextStoryLine() {
+    if (storyLines.length == 0 || curStoryIdx < 0) {
+      storyBoxOverlay.removeEventListener("click", viewNextStoryLine);
+      return;
+    }
+    if (curStoryIdx < storyLines.length) {
+      readStoryByOneLine();
+      return;
+    }
+
+    // if the last story line has enabled "loop" = true, the story box will never disappear
+    const curStoryLine = storyLines[curStoryIdx - 1];
+    if ("loop" in curStoryLine && curStoryLine["loop"] === true) {
+      curStoryIdx--;
+    } else {
+      hideStoryBox();
+      storyBoxOverlay.removeEventListener("click", viewNextStoryLine);
+    }
   }
 
-  fetch(`../../../assets/storylines/${storyPath}`)
-    .then((response) => response.json())
-    .then((data) => {
-      storyLines = data;
+  function readStoryByOneLine() {
+    if (curStoryIdx >= storyLines.length) {
+      return;
+    }
+    storyText.textContent = storyLines[curStoryIdx]["text"];
+    curStoryIdx++;
+  }
 
-      storyText.textContent = storyLines[curStoryIdx]["text"];
-      curStoryIdx++;
+  readStoryByOneLine();
+  storyBoxOverlay.addEventListener("click", viewNextStoryLine);
 
-      setTimeout(() => {
-        storyBox.style.visibility = "visible";
+  showStoryBox();
+}
 
-        storyBox.addEventListener("click", function () {
-          if (storyLines.length == 0 || curStoryIdx < 0) {
-            return;
-          }
-          if (curStoryIdx < storyLines.length) {
-            storyText.textContent = storyLines[curStoryIdx]["text"];
-            curStoryIdx++;
-          } else {
-            // if the last story line has enabled "loop" = true, the story box will never disappear
-            const curStoryLine = storyLines[curStoryIdx - 1];
-            if ("loop" in curStoryLine && curStoryLine["loop"] === true) {
-              curStoryIdx--;
-            } else {
-              storyBox.style.display = "none";
-            }
-          }
-        });
-      }, 2000);
-    })
-    .catch((error) => console.error("Error loading JSON:", error));
-});
+function isStoryLineEnd(storyLines) {
+  return curStoryIdx >= storyLines.length;
+}
+
+function showStoryBox() {
+  if (storyBox == null) {
+    return;
+  }
+  storyBox.style.visibility = "visible";
+  if (storyBoxOverlay == null) {
+    return;
+  }
+  storyBoxOverlay.style.visibility = "visible";
+}
+
+function hideStoryBox() {
+  if (storyBox == null) {
+    return;
+  }
+  storyBox.style.visibility = "hidden";
+
+  if (storyBoxOverlay == null) {
+    return;
+  }
+  storyBoxOverlay.style.visibility = "hidden";
+}
