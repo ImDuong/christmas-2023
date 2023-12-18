04e1b0b201de6360a9648be7f224175b44649634
ImDuong <ImDuong@users.noreply.github.com>
2023-12-18 17:14:43 +0000

    deploy: 3eb531d8ebbb675babf4f1976717fdc880a6a04f

---
+++ dist/rooms/room-happiness/index.js
@@ -0,0 +1,18 @@
+import {loadNextRoom} from '../../components/door/door.js'
+
+// set path to intial story lines for storybox
+const intialStoryPath = "demo/room-happiness.json";
+const rightRoomStoryPath = "navigation/happiness-room-right-door.json";
+
+document.addEventListener("DOMContentLoaded", async function () {
+  var intialStoryLines = await fetchStoryLines(intialStoryPath);
+  setTimeout(() => {
+    viewStoryLines(intialStoryLines);
+  }, 1500);
+
+  loadNextRoom(
+    rightRoomStoryPath,
+    "right-door-wrapper",
+    ""
+  );
+});
