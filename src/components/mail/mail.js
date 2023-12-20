4761e96bcf1f7621547223ca4c80f0114f26786a
namikemen <namdt2132@icloud.com>
2023-12-20 20:57:01 +0700

    add mail to suffering room

---
+++ src/components/mail/mail.js
@@ -0,0 +1,46 @@
+import { navigate } from "@@webRoot/utils/utils.js";
+
+/**
+ * @description Load the next room in the interactive storytelling game when the door is clicked.
+ * It fetches the storylines for the current room using the provided storyPath. The function adds a click event listener to the door,
+ * allowing the player to view the storylines and, once finished, navigate to the next room.
+ *
+ * Special cases:
+ * 1. If there are no storylines, it immediately navigates to the specified roomURI.
+ * 2. If the roomURI is empty, it signifies that the player cannot leave the room.
+ *
+ * @param {string} storyPath - The relative path (in perspective of assets/storylines folder) to fetch the storylines for the current room.
+ * @param {string} doorWrapperDOMElementID - The ID of the HTML element wrapping the door.
+ * @param {string} roomURI - The URI of the next room to navigate to when the storylines end.
+ *
+ * @example
+ * loadNextRoom("navigation/main-room-left-door.json", "left-door-wrapper", "@@webRoot/rooms/room-happiness/index.html");
+ */
+export function loadPuzzle(storyPath, doorWrapperDOMElementID, roomURI) {
+  if (storyPath.length == 0) {
+    navigate(roomURI);
+  }
+
+  var storyLines = [];
+  var isRoomEnterable = false;
+  fetchStoryLines(storyPath)
+    .then((fetchedSl) => {
+      storyLines = fetchedSl;
+    })
+    .catch((e) => {
+      console.error(`Fetch story lines from ${storyPath} failed:`, e);
+    });
+
+  var doorWrapper = document.getElementById(doorWrapperDOMElementID);
+  doorWrapper.addEventListener("click", function () {
+    if (!isRoomEnterable) {
+      viewStoryLines(storyLines);
+      if (roomURI.length > 0) {
+        isRoomEnterable = true;
+      }
+      return;
+    }
+
+    navigate(roomURI);
+  });
+}
--- src/rooms/room-suffering/index.js
+++ src/rooms/room-suffering/index.js
@@ -3,6 +3,9 @@ import {loadNextRoom} from '@@webRoot/components/door/door.js'
 // set path to intial story lines for storybox
 const intialStoryPath = "demo/room-suffering.json";
 const leftRoomStoryPath = "navigation/suffering-room-left-door.json";
+const mailPath = "@@webRoot/components/mail/mail.html";
+const mainRoomPath = "@@webRoot/rooms/main-room/index.html";
+const puzzlePath = "@@webRoot/puzzle/crossword/index.html";
 
 document.addEventListener("DOMContentLoaded", async function () {
   var intialStoryLines = await fetchStoryLines(intialStoryPath);
@@ -10,9 +13,25 @@ document.addEventListener("DOMContentLoaded", async function () {
     viewStoryLines(intialStoryLines);
   }, 1500);
 
+  function loadMail() {
+    setTimeout(async function() {
+      const response = await fetch(mailPath);
+      const htmlContent = await response.text();
+      document.body.innerHTML += htmlContent;
+    }, 10000); // 10000 milliseconds = 10 seconds
+  }
+  loadMail();
+  loadNextRoom(
+    leftRoomStoryPath,
+    "letter-wrapper",
+    puzzlePath
+  );
+
   loadNextRoom(
     leftRoomStoryPath,
     "left-door-wrapper",
-    "@@webRoot/rooms/main-room/index.html"
+    mainRoomPath
   );
+
+  
 });
